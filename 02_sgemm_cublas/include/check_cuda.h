33c26929bff23bda3e4bd48c475cb1693d714c83
coriouslien <chialan.lien@gmail.com>
2026-08-18 13:46:56 -0400

    Update check_cuda.h

--- 02_sgemm_cublas/include/check_cuda.h
+++ 02_sgemm_cublas/include/check_cuda.h
@@ -1,1 +1,25 @@
+// check_cuda.h
+
+#pragma once
+
+#include <cuda_runtime.h>
+#include <cstdio>
+#include <cstdlib>
+
+// marco for clean CUDA error checking
+#define CHECK_CUDA(ans)                        \
+    {                                          \
+        cudaAssert((ans), __FILE__, __LINE__); \
+    }
+inline void cudaAssert(cudaError_t code, const char *file, int line, bool abort = true)
+{
+    if (code != cudaSuccess)
+    {
+        fprintf(stderr, "GPU assrt: %s %s %d\n", cudaGetErrorString(code), file, line);
+        if (abort)
+        {
+            exit(code);
+        }
+    }
+}
 
