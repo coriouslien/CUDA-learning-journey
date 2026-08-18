0ed67d4a25e626111afbf6c5c3d80633da893c2a
coriouslien <chialan.lien@gmail.com>
2026-08-18 13:44:35 -0400

    Update check_cublas.h

--- 02_sgemm_cublas/include/check_cublas.h
+++ 02_sgemm_cublas/include/check_cublas.h
@@ -1,1 +1,22 @@
 
+// check_cublas.h
+#pragma once
+
+#include <cublas_v2.h>
+#include <cstdio>
+
+#define CHECK_CUBLAS(ans)                        \
+    {                                            \
+        cublasAssert((ans), __FILE__, __LINE__); \
+    }
+inline void cublasAssert(cublasStatus_t code, const char *file, int line, bool abort = true)
+{
+    if (code != CUBLAS_STATUS_SUCCESS)
+    {
+        fprintf(stderr, "cuBLAS assert: code &d in %d %s %d\n", code, file, line);
+        if (abort)
+        {
+            exit(code);
+        }
+    }
+}
