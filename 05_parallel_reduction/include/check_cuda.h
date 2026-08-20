563af5f1256d05cb9ff54ba784e973065c91c614
coriouslien <chialan.lien@gmail.com>
2026-08-20 09:16:38 -0400

    Create check_cuda.h

---
+++ 05_parallel_reduction/include/check_cuda.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdio>
+#include <cuda_runtime.h>
+
+#define CHECK_CUDA(call)                                   \
+    do                                                     \
+    {                                                      \
+        cudaError_t err = call;                            \
+        if (err != cudaSuccess)                            \
+        {                                                  \
+            printf("CUDA Error: %s - at %s at line %d:\n", \
+                   cudaGetErrorString(err),                \
+                   __FILE__,                               \
+                   __LINE__);                              \
+            exit(EXIT_FAILURE);                            \
+        }                                                  \
+    } while (0)
